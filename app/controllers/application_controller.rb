4f90b521b092cac9d7f1698156d061f3912e17d0
thebevrishot <panu.suk@satang.com>
2019-05-20 14:37:34 +0700

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   def hi
     render html: 'hi, friends'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
@@ -7,5 +9,10 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
